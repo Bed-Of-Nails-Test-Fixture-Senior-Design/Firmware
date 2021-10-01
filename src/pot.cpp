0125ae2d2546a0c32af7f497c253f6a052c6de73
OmarAlmatar <osalmatar@sdsu.edu>
2021-09-30 17:29:59 -0700

    Slight modif to pot h

--- src/pot.cpp
+++ src/pot.cpp
@@ -1,28 +1,15 @@
 #include "../include/pot.h"
 
 // Writes to digital outputs to control potentiometer relays
-POT::POT(int inCCW, int inMID, int inCW{	//constructor POT
-    inCCW = CCW;
-    inMID = MID;
-    inCW = CW;
+POT::POT(int inCCW, int inMID, int inCW) {	//constructor POT
     pinMODE(inCCW, OUTPUT);
     pinMODE(inMID, OUTPUT);
     PINMODE(inCW, OUTPUT);
-}
-
-
-void POT::set_CCW(){
-    digitalWrite(CCW,1);
-    digitalWrite(MID,0);
-    digitalWrite(CW, 0);
-    state = "CCW";
-}
-
-void POT::set_MID(){
-    digitalWrite(CCW,1);
-    digitalWrite(MID,1);
-    digitalWrite(CW, 0);
-    state = "MID";
+    
+    inCCW = CCW;
+    inMID = MID;
+    inCW = CW;
+    
 }
 
 void POT::set_CW(){
@@ -30,4 +17,18 @@ void POT::set_CW(){
     digitalWrite(MID,0);
     digitalWrite(CW, 1);
     state = "CW";
+}
+
+void POT::set_MID(){
+    digitalWrite(CCW,0);
+    digitalWrite(MID,1);
+    digitalWrite(CW, 0);
+    state = "MID";
+}
+
+void POT::set_CCW(){
+    digitalWrite(CCW,1);
+    digitalWrite(MID,0);
+    digitalWrite(CW, 0);
+    state = "CCW";
 }
